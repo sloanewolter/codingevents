48db17e067353bfdcf105bbdd60d6a5521880a2b
Sloane Wolter <sloanewolter@gmail.com>
2023-10-16 13:51:47 -0500

    finished exercises on validation

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,89 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.*;
+
+import java.util.Date;
 import java.util.Objects;
 
 public class Event {
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
+    @Size(max = 500, message = "Description too long")
     private String description;
+
+    @Email(message = "Invalid email, try again.")
+    @NotBlank
+    private String contactEmail;
+
+    @NotBlank
+    @NotNull
+    private String location;
+
+    @AssertTrue(message="You must be required to register for the event.")
+    private boolean mustRegister;
+
+    @DecimalMin(value= "1", message = "Number must be greater than 0.")
+    private int numberOfAttendees;
+
+    @Future(message="The event must take place in the future.")
+    private Date date;
 
     private int id;
     private static int nextId = 1;
 
-    public Event(String name, String description) {
+    public Event() {}
+    public Event(String name, String description, String contactEmail, String location, boolean mustRegister, int numberOfAttendees,Date date) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.mustRegister = mustRegister;
+        this.numberOfAttendees = numberOfAttendees;
+        this.date= date;
         this.id=nextId;
         nextId++;
+    }
+
+    public boolean isMustRegister() {
+        return mustRegister;
+    }
+
+    public void setMustRegister(boolean mustRegister) {
+        this.mustRegister = mustRegister;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
